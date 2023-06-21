82375054ca0ca320d8ff1fe25d98d3a53313f3fa
Mark <bf-109g2@hotmail.com>
2023-06-21 10:47:21 -0400

    Custom Prompts

--- page_qa.py
+++ page_qa.py
@@ -3,11 +3,15 @@ import logging
 from typing import List, Optional
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.question_answering import _load_map_reduce_chain
 from langchain.chains import AnalyzeDocumentChain
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
-
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
 
 load_dotenv()
 logger = logging.getLogger(__name__)
@@ -82,7 +86,8 @@ class URLLoader(BaseLoader):
 
                     page_source = page.content()
                     # readabilipy is used to remove scripts and styles
-                    simple_tree = simple_tree_from_html_string(page_source)
+                    # simple_tree = simple_tree_from_html_string(page_source)
+                    simple_tree = page_source
                     # html2text is used to convert html to markdown
                     text = html2text(str(simple_tree))
                     metadata = {"source": url}
@@ -98,24 +103,45 @@ class URLLoader(BaseLoader):
         return docs
 
 
-# change this for your use case
-url = "https://www.amazon.com/Stanley-Quick-Flip-GO-Bottle/dp/B08RXT2YVL?ref_=ast_sto_dp&th=1&psc=1"
-query = "What is the price of the bottle?"
+system_template = """Use the following portion of a long document to see if any of the text is relevant to answer the question. 
+Return any relevant text verbatim. If there is no relevant text, return an empty string.
+______________________
+{context}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+CHAT_QUESTION_PROMPT = ChatPromptTemplate.from_messages(messages)
 
 
-def answer(query: str, 
-           url: Optional[str] = None, 
-           filename: Optional[str] = None,
-           output_llm_input: bool = False):
+system_template = """Given the following extracted parts of a long document and a question, create a final answer. 
+If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+______________________
+{summaries}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+CHAT_COMBINE_PROMPT = ChatPromptTemplate.from_messages(messages)
+
+
+def answer(
+    query: str,
+    url: Optional[str] = None,
+    filename: Optional[str] = None,
+    output_llm_input: bool = False,
+):
     """Answer a question from reading a website"""
 
-    assert (url is None) ^ (filename is None), "Exactly one of url or html_path must be provided"
-    
+    assert (url is None) ^ (
+        filename is None
+    ), "Exactly one of url or html_path must be provided"
+
     if url:
         loader = URLLoader(urls=[url], headless=True)
         page_content = loader.load()[0].page_content
     elif filename:
-        with open(filename, 'r') as f:
+        with open(filename, "r", encoding="utf-8") as f:
             page_content = f.read()
     else:
         # unreachable code but use else branch to fix linter complaint
@@ -126,22 +152,30 @@ def answer(query: str,
 
     llm = ChatOpenAI()  # type: ignore
 
-    qa_chain = load_qa_chain(llm, chain_type="map_reduce")
+    qa_chain = _load_map_reduce_chain(
+        llm,
+        question_prompt=CHAT_QUESTION_PROMPT,
+        combine_prompt=CHAT_COMBINE_PROMPT,
+        verbose=True,
+    )
     qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=qa_chain)
     output = qa_document_chain(
         {"input_document": page_content, "question": query},
         return_only_outputs=True,
     )
-    print("Answer:", output['output_text'])
+    print("Answer:", output["output_text"])
 
 
-def download(url: str, 
-             filename: str,
-             output_content: bool = False):
+def download(url: str, filename: str, output_content: bool = False):
     """Download a webpage to a file"""
-    loader = URLLoader(urls=[url], headless=True)
+    loader = URLLoader(
+        urls=[url],
+        headless=True,
+        remove_selectors=["script", "style"],
+        continue_on_failure=False,
+    )
     page_content = loader.load()[0].page_content
-    with open(filename, 'w') as f:
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(page_content)
     if output_content:
         print(page_content)
@@ -150,8 +184,4 @@ def download(url: str,
 if __name__ == "__main__":
     import fire
 
-    fire.Fire({
-            'ans': answer,
-            'dl': download
-        })
-
+    fire.Fire({"ans": answer, "dl": download})
